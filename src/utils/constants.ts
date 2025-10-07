0962d7b0d911b3fe41e073233708d27f17c09fc4
Ilya Shustenkov <siv_2003@mail.ru>
2025-10-07 05:53:46 +0300

    chore: update github link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const procharityURL = 'https://procharity.ru/';
 export const myWorkMail = 'shustenkovvv@gmail.com';
-export const myGithubURL = 'https://github.com/Shustenkov';
+export const myGithubURL = 'https://github.com/madanimatronic';
 export const devTitle = 'Shustenkov Dev';
 export const hhResumeURL =
   'https://hh.ru/resume/e3d8a0f2ff0ec2d0320039ed1f415834646a59';
